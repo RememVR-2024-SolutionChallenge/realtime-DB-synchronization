d256e153a5704fbe52ba914e6824a01f5f0fcf50
woog2roid <woog2roid@gmail.com>
2024-02-15 02:27:44 +0900

    feat: Implement auto-sync logic for firestore-mysql

---
+++ index.js
@@ -0,0 +1,57 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const mysql = require("mysql");
+
+require("dotenv").config();
+
+// Initialize Firebase admin SDK.
+// Omit deatiled settings, becasue of same GCP project.
+admin.initializeApp();
+
+// Initalize MySQL settings.
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Trigger this function when new document adds in Firestore's collection.
+exports.syncFirestoreToMySQL = functions.firestore
+  .document(`${process.env.FIRESTORE_COLLECTION}/{docId}`)
+  .onCreate((snap, context) => {
+    const newData = snap.data();
+
+    // Logging.
+    console.log(`New Firestore document(${context.params.docId}): `, newData);
+
+    // Generate SQL query.
+    const sql = `
+        INSERT INTO vr_resource (id, title, type, filePath, createdAt, groupId)
+        VALUES (?, ?, ?, ?, ?, ?)
+      `;
+
+    const values = [
+      context.params.docId,
+      newData.title,
+      newData.type,
+      newData.filePath,
+      newData.createdAt,
+      newData.groupId,
+    ];
+
+    // Insert data to MySQL.
+    return new Promise((resolve, reject) => {
+      pool.query(sql, values, (error, results, fields) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        } else {
+          console.log("Inserted data into MySQL", results);
+          resolve(results);
+        }
+      });
+    });
+  });
